4d277bdcc8b741aa4c883cc668b17d9e68b9ffb7
Manjul Tamrakar <manjultamrakar4@gmail.com>
2024-06-29 16:55:51 +0545

    finish room1 map

--- src/scenes/room1.js
+++ src/scenes/room1.js
@@ -3,6 +3,10 @@ import { setBackgroundColor, setMapColliders } from "./roomUtils.js";
 export function room1(k, roomData) {
     setBackgroundColor(k, "#a2aed5");
 
+    k.camScale(4);
+    k.camPos(170, 100);
+    k.setGravity(1000);
+
     const roomLayers = roomData.layers;
 
     const map = k.add([k.pos(0, 0), k.sprite("room1")]);
